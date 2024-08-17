d8da9e3f8d92f577439dd93ca361276c655a529a
win <2296627807@qq.com>
2024-08-17 19:38:21 +0800

    test

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int n =in.nextInt();
-        String s = in.next();
+        int[] arr = new int[n];
+        long sum = 0;
         for(int i=0;i<n;i++){
-            if(s.charAt(i)=='?'){
-                System.out.println(n-i);
-                return;
-            }
+            arr[i] = in.nextInt();
+            sum += arr[i];
         }
+
+        long tar = sum/n;
+        long ans = 0;
+        for(int i=0;i<n;i++){
+            ans += (Math.abs(arr[i]-tar));
+        }
+
+        System.out.println(ans/2);
+
     }
 }

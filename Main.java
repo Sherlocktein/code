02ef57cb40618bac28804a616ae1a80706119e86
win <2296627807@qq.com>
2024-08-17 20:10:28 +0800

    test

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     static long tar = 0;
     static boolean tf = false;
-    public static boolean f(int x){
+    public static boolean f(long x){
         if(x==tar) return true;
         if(!tf && x==(tar+1)) return true;
         return false;
@@ -12,19 +12,19 @@ public class Main {
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n =in.nextInt();
-        int[] arr = new int[n];
+        long n = in.nextLong();
+        long[] arr = new long[(int)n];
         long sum = 0;
         for(int i=0;i<n;i++){
-            arr[i] = in.nextInt();
-            sum += arr[i];
+            arr[i] = in.nextLong();
+            sum += (long) arr[i];
         }
 
-        tar = sum/n;
-        tf = (sum%n==0);
+        tar = sum / (long) n;
+        tf = (sum % (long) n == 0);
         Arrays.sort(arr);
 
-        int i=0,j=n-1;
+        int i=0,j=(int)n-1;
         long ans = 0;
         while(i<j){
             while(i<j && f(arr[i])) i++;
@@ -37,7 +37,7 @@ public class Main {
             }
         }
         if(!tf){
-            if(arr[i]!=tar) ans+=(arr[i]-(tar+1));
+            if(arr[i]!=tar) ans += (long)(arr[i]-(tar+1));
         }
         System.out.println(ans);
     }

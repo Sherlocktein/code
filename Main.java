e2ee2cdaa5dc4fa0722a8db7dadc5d2ec58dbf8a
win <2296627807@qq.com>
2024-08-23 20:20:21 +0800

    test

--- Main.java
+++ Main.java
@@ -1,50 +1,57 @@
-import java.awt.*;
+import java.util.ArrayList;
 
-class ListNode{
+class TreeNode{
     int val;
-    ListNode next = null;
-    public ListNode(int val){
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val){
         this.val=val;
     }
 }
 
-public class Solution {
-    public int merge(ListNode a) {
-        if(a.next==null) return 0;
-        int ans = 0;
+class Tuple{
+    int no;
+    TreeNode node;
+    public Tuple(int no,TreeNode node){
+        this.no=no;
+        this.node=node;
+    }
+}
 
-        ListNode cur = a;
-        while(cur.next!=null){
-            if(cur.val == 0){
-                if(cur.next.val==0) {
-                    ans++;
-                    cur = cur.next;
-                }else{
-                    cur=cur.next;
-                }
-            }else{
-                if(cur.next.val==0){
-                    cur=cur.next;
-                }else {
-                    ans++;
-                    cur=cur.next;
-                }
+public class Solution {
+
+    int no = 0;
+    public void dfs(TreeNode root,int curno){
+        no = Math.max(no,curno);
+        if(root.left!=null) dfs(root.left,curno*2);
+        if(root.right!=null) dfs(root.right,curno*2+1);
+    }
+    public TreeNode makeCompleteTree(TreeNode root) {
+        dfs(root,1);
+
+        ArrayList que = new ArrayList<Tuple>();
+        que.add(new Tuple(1,root));
+
+
+        while(que.size()>0){
+            Tuple tuple = (Tuple) que.get(0);
+            que.remove(0);
+
+            int curno = tuple.no;
+            TreeNode node = tuple.node;
+
+            if(curno*2<no){
+                if(node.left==null) node.left=new TreeNode(1);
+                que.add(new Tuple(curno*2,node.left));
             }
+            if((curno*2+1)<no){
+                if(node.right==null) node.right=new TreeNode(1);
+                que.add(new Tuple(curno*2+1,node.left));
+            }
+
         }
 
-        return ans;
-    }
-
-    public static void main(String[] args){
-        ListNode node = new ListNode(0);
-        ListNode cur = node;
-        cur.next = new ListNode(0);
-        cur=cur.next;
-        cur.next=new ListNode(0);
-
-        Solution s = new Solution();
-
-        System.out.println(s.merge(node));
+        return root;
     }
 
 }

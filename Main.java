28bc851091a3f7261df7eabed93e4a9d079c3df0
win <2296627807@qq.com>
2024-08-23 19:25:47 +0800

    test

--- Main.java
+++ Main.java
@@ -1,16 +1,50 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.awt.*;
 
-public class Main {
-    static long tar = 0;
-    static boolean tf = false;
-    public static boolean f(long x){
-        if(x==tar) return true;
-        if(!tf && x==(tar+1)) return true;
-        return false;
+class ListNode{
+    int val;
+    ListNode next = null;
+    public ListNode(int val){
+        this.val=val;
+    }
+}
+
+public class Solution {
+    public int merge(ListNode a) {
+        if(a.next==null) return 0;
+        int ans = 0;
+
+        ListNode cur = a;
+        while(cur.next!=null){
+            if(cur.val == 0){
+                if(cur.next.val==0) {
+                    ans++;
+                    cur = cur.next;
+                }else{
+                    cur=cur.next;
+                }
+            }else{
+                if(cur.next.val==0){
+                    cur=cur.next;
+                }else {
+                    ans++;
+                    cur=cur.next;
+                }
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args){
+        ListNode node = new ListNode(0);
+        ListNode cur = node;
+        cur.next = new ListNode(0);
+        cur=cur.next;
+        cur.next=new ListNode(0);
 
+        Solution s = new Solution();
+
+        System.out.println(s.merge(node));
     }
+
 }

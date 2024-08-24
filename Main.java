1fe97a8f245ad3d84ebaa5a96d7d1e6525a951fa
win <2296627807@qq.com>
2024-08-24 16:52:21 +0800

    test

--- Main.java
+++ Main.java
@@ -1,53 +1,49 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class Tuple{
-    int no;
-    TreeNode node;
-    public Tuple(int no,TreeNode node){
-        this.no=no;
-        this.node=node;
-    }
-}
-
-public class Solution {
-
-    int no = 0;
-    public void dfs(TreeNode root,int curno){
-        no = Math.max(no,curno);
-        if(root.left!=null) dfs(root.left,curno*2);
-        if(root.right!=null) dfs(root.right,curno*2+1);
-    }
-    public TreeNode makeCompleteTree(TreeNode root) {
-        dfs(root,1);
-
-        ArrayDeque que = new ArrayDeque<Tuple>();
-        que.add(new Tuple(1,root));
-
-        while(que.size()>0){
-            Tuple tuple = (Tuple) que.poll();
-
-            int curno = tuple.no;
-            TreeNode node = tuple.node;
-
-            if(curno*2<no){
-                if(node.left==null){
-                    node.left=new TreeNode(curno*2);
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(),m = in.nextInt(),k = in.nextInt();
+        int[][] arr = new int[n+1][m+1];
+        in.nextLine();
+        for(int i = 0;i<k;i++){
+            String c = in.nextLine();
+            String op = c.split(" ")[0];
+            int x = Integer.parseInt(c.split(" ")[1]), y = Integer.parseInt(c.split(" ")[2]);
+            if(op.equals("c")){
+                arr[x][y] = 1;
+            }else if(op.equals("l")){
+                int j = y-1;
+                for(;j>=1;j--){
+                    if(arr[x][j]==0) break;;
                 }
-                que.add(new Tuple(curno*2,node.left));
-            }
-            if((curno*2+1)<no){
-                if(node.right==null){
-                    node.right=new TreeNode(curno*2+1);
+                if(j>=1) System.out.println(x + " " + j);
+                else System.out.println(-1);
+            }else if(op.equals("r")){
+                int j = y+1;
+                for(;j<=m;j++){
+                    if(arr[x][j]==0) break;;
                 }
-                que.add(new Tuple(curno*2+1,node.right));
+                if(j<=m) System.out.println(x + " " + j);
+                else System.out.println(-1);
+            }else if(op.equals("u")){
+                int j = x-1;
+                for(;j>=1;j--){
+                    if(arr[j][y]==0) break;;
+                }
+                if(j>=1) System.out.println(j + " " + y);
+                else System.out.println(-1);
+            }else{
+                int j = x+1;
+                for(;j<=n;j++){
+                    if(arr[j][y]==0) break;;
+                }
+                if(j<=n) System.out.println(j + " " + y);
+                else System.out.println(-1);
             }
 
         }
-        System.out.println(num);
-        return root;
+
     }
-
-
 }
